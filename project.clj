24775f233a7ceac60ef2555531d4b0df673662a2
Joshua F. Mathews <JoshFeltonM@gmail.com>
2020-04-04 10:57:55 -0500

    got compojure working with ring

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring "1.8.0"]]
+                 [ring "1.8.0"]
+                 [compojure "1.6.1"]]
   :repl-options {:init-ns clojure-blog.core}
-  :main clojure-blog.core)
+  :main clojure-blog.core
+  :profiles {:dev
+             {:main clojure-blog.core/-dev-main}})
--- src/clojure_blog/core.clj
+++ src/clojure_blog/core.clj
@@ -1,14 +1,30 @@
 (ns clojure-blog.core
-  (:require [ring.adapter.jetty :as webserver]))
+  (:require [ring.adapter.jetty :as webserver]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
+
+(defn welcome
+  [request]
+    {:status 200
+     :body "<h1>Learning ring and compojre  now.</h1>"
+     :headers {}})
+
+(defroutes app
+  (GET "/" [] welcome)
+  (not-found "<h1>Unable to find this page.</h1>"))
+
+(defn -dev-main
+  "Just getting started..."
+  [port-number]
+  (webserver/run-jetty
+                       (wrap-reload #'app)
+                       {:port (Integer. port-number)
+                        :join? false}))
 
 (defn -main
   "Just getting started..."
   [port-number]
   (webserver/run-jetty
-                       (fn [request]
-                         {:status 200
-                          :headers {}
-                          :body "<h1> Yo, does this work? </h1>"
-                          })
-                       {:port (Integer. port-number)
-                        :join? false}))
+                      app
+                       {:port (Integer. port-number)}))
