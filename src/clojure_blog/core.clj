e09fbbcba78523a8e78b575128b5b5a9fc5eb5ba
Joshua F. Mathews <JoshFeltonM@gmail.com>
2020-04-04 00:19:05 -0500

    basic ring endpoint

--- src/clojure_blog/core.clj
+++ src/clojure_blog/core.clj
@@ -1,6 +1,14 @@
-(ns clojure-blog.core)
+(ns clojure-blog.core
+  (:require [ring.adapter.jetty :as webserver]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  "Just getting started..."
+  [port-number]
+  (webserver/run-jetty
+                       (fn [request]
+                         {:status 200
+                          :headers {}
+                          :body "<h1> Yo, does this work? </h1>"
+                          })
+                       {:port (Integer. port-number)
+                        :join? false}))
